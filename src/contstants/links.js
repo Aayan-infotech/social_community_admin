bde0ce41df6f08fa3ef35c6790e58142edcdf912
Ankul Yadav <ankul.yadav@aayaninfotech.com>
2025-07-14 11:50:45 +0530

    updated the ip

--- src/contstants/links.js
+++ src/contstants/links.js
@@ -1,6 +1,6 @@
 const links = {
-  // BASE_URL: 'http://18.209.91.97:3030/api/',
-  BASE_URL: 'http://localhost:3030/api/',
+  BASE_URL: 'http://52.20.55.193:3030/api/',
+  // BASE_URL: 'http://localhost:3030/api/',
 };
 
 export default links;
