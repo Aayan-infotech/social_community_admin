990e62fba11688850b7f4703db398b7275d99f91
Ankul Yadav <ankul.yadav@aayaninfotech.com>
2025-08-12 18:11:04 +0530

    change the base url

--- src/contstants/links.js
+++ src/contstants/links.js
@@ -1,6 +1,6 @@
 const links = {
-  // BASE_URL: 'http://3.230.209.171:3030/api/',
-  BASE_URL: 'http://localhost:3030/api/',
+  BASE_URL: 'http://3.230.209.171:3030/api/',
+  // BASE_URL: 'http://localhost:3030/api/',
 };
 
 export default links;
