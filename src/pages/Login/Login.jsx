6c9680bd7e768570300a7dd36f6b561b719d5a52
Ankul Yadav <ankul.yadav@aayaninfotech.com>
2025-08-23 16:50:16 +0530

    added the appversion header

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -34,6 +34,7 @@ const LoginPage = () => {
       const response = await axios.post("auth/login", form, {
         headers: {
           "Content-Type": "application/json",
+          "appVersion":"45"
         },
       });
 
